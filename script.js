d70c842c0614b2a1cbdd7111ea084855ca3f4c40
Sergey Shishkin <sergejshishkin@gmail.com>
2022-12-11 18:34:15 +0300

    removed js file

--- script.js
+++
@@ -1,8 +0,0 @@
-/*
-function callModalWindow() {
-  alert("Кнопка нажата");
-}
-
-let callModalBtn = document.getElementById("bannerbtn");
-callModalBtn.addEventListener("click", callModalWindow);
-*/
